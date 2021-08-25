f5559964fafbfebbfadd369c6bb7ec074ba3c0a7
Titan <lildzii.wd@gmail.com>
2021-08-25 12:49:15 +0000

    update

--- code/src/resources/item.py
+++ code/src/resources/item.py
@@ -5,44 +5,32 @@ from src.controller.item import ItemController
 from flask_restful import Resource
 from flask import request, g
 
-
-"""
-Item Resouce class
-
-"""
-
 class ItemResource(Resource):
     """ ItemResouce class contains methods for getting, deleting and updating single item ITem """
 
-    def __init__(self):
-        """ Initialise Repository and controller variable neccessary for method action performance """
-        self.controller = ItemController()
-
     @Authenticate.is_authenticated_or_authorised()
     def get(self, id: str):
-        return self.controller.get_by_id(id)
+        return ItemController().get_by_id(id)
 
     @Authenticate.is_authenticated_or_authorised()
     @Validator.validate(validator=ItemPatchBodyValidation())
     def patch(self, id: str):
-            return self.controller.update(id, **data)
+        return ItemController().update(id, **data)
 
     @Authenticate.is_authenticated_or_authorised()
     def delete(self, id: str):
-            return self.controller.delete(id)
+        return ItemController().delete(id)
 
 
 class ItemListResource(Resource):
     """ ItemListResouce class contains methods for getting and creating Item resouce """
-    def __init__(self):
-        self.controller = ItemController()
-            
+
     @Authenticate.is_authenticated_or_authorised()
     @Validator.validate(ItemParamsValidation(), validation_data="params")
     def get(self):
-            return self.controller.get_docs(**g.params)
+        return ItemController().get_docs(**g.params)
 
     @Authenticate.is_authenticated_or_authorised()
     @Validator.validate(ItemBodyValidation())
     def post(self):
-            return self.controller.insert(**g.body)
+        return ItemController().insert(**g.body)
