87bc7194f0fead8f91287d10109ca36b77522a82
Titan <lildzii.wd@gmail.com>
2021-08-26 08:27:59 +0000

    update

--- code/src/controller/base.py
+++ code/src/controller/base.py
@@ -31,9 +31,7 @@ class BaseController:
     def get_docs(self, **query: Dict) -> Dict:
         try:
             data = self.repository.get_docs(**query)
-            if data:
-                return self.response.successWithData(data=data, message=f"{self.name} fetched succesfully", statusCode=200)
-            return self.response.error(message="No record found", statusCode=404), 404
+            return self.response.successWithData(data=data or [], message=f"{self.name} fetched succesfully", statusCode=200)
         except Exception as error:
             app.logger.error(error)
             return self.response.error(message=str(error), statusCode=400), 400
@@ -43,7 +41,7 @@ class BaseController:
             data = self.repository.update(id=id, **payload)
             if data:
                 return self.response.successWithData(data=data, message=f"{self.name} updated successfully", statusCode=200)
-            return self.response.error(message="Error occurred", statusCode=404), 404
+            return self.response.error(message="No record found", statusCode=404), 404
         except Exception as error:
             app.logger.error(error)
             return self.response.error(message=str(error), statusCode=400), 400
@@ -53,7 +51,7 @@ class BaseController:
             data = self.repository.delete(id=id)
             if data:
                 return self.response.successWithData(data=data, message=f"{self.name} deleted successfully", statusCode=200)
-            return self.response.error(message="Error occured while deleting", statusCode=404)
+            return self.response.error(message="No record found", statusCode=404), 404
         except Exception as error:
             app.logger.error(error)
             return self.response.error(message=str(error), statusCode=400), 400
