f9f67d6d7fed53dcbd0e9866b8525cd31c7ce0f2
Certified Lover Boy <workwithwhitson@gmail.com>
2021-09-04 23:34:09 +0000

    update

--- code/app.py
+++ code/app.py
@@ -5,8 +5,6 @@ from src import app
 
 # Handle generic request exceptions here
 
-
-
 if __name__ == '__main__':
     app.logger.info('App running debug: {}'.format(CONFIG.DEBUG))
     app.run(port=CONFIG.PORT, host=CONFIG.HOST)
